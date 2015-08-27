cb0160f48e605ff73e8a7dd6487ddac79d056d06
crazyjingling <crazyjingling@126.com>
2015-08-27 18:21:21 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 
 /*
  * Create variables for our project paths so we can change in one place
@@ -25,4 +26,16 @@ gulp.task('watch:lint', function () {
 		.pipe(watch())
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter));
+});
+
+// gulp watcher for sass
+gulp.task('watch:sass', ['sass'], function () {
+	gulp.watch('./public/styles/**/*.scss', [
+		'sass'
+	]);
+});
+
+// sass任务：将.scss文件转换为css,放在stylesheets下
+gulp.task('sass', function () {
+	gulp.src('./public/styles/site.scss').pipe(sass.sync({ indentedSyntax: true }).on('error', sass.logError)).pipe(gulp.dest('./public/styles/'));
 });
--- routes/views/index.js
+++ routes/views/index.js
@@ -4,12 +4,86 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+	locals.data = {
+		cards: [{
+			title: 'card1',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的加哈卡士大夫很快恢复快圣诞节发货户籍卡货到付款吉安市福建省会计法哈克说接电话发顺丰阿斯顿回复接口撒谎都放假哈SD卡发哈SD卡交话费撒旦法很快就到会发生空间地方好家伙接收到回复看',
+			createdAt: '2015.08.27'
+		},{
+			title: 'card2',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的加哈卡回复看',
+			createdAt: '2015.08.27'
+		},{
+			title: 'card3',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的付款吉安市福建省会计法哈克说接电话发顺丰阿斯顿回复接口撒谎都放假哈SD卡发哈SD卡交话费撒旦法很快就到会发生空间地方好家伙接收到回复看',
+			createdAt: '2015.08.27'
+		},{
+			title: 'card4',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的加哈卡士大夫很快恢复快圣诞节发货户籍卡货到付款吉安市福建省会计法哈克说接电话发顺丰阿斯顿回复接口撒谎都放假哈SD卡发哈SD卡交话费撒旦法很快就到会发生空间地方好家伙接收到回复看',
+			createdAt: '2015.08.27'
+		}],
+		sliders: [{
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+		},{
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+		},{
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+		},{
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+		}],
+		user: {
+			images: [{
+				url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+				image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			},{
+				url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+				image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			},{
+				url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+				image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			},{
+				url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+				image: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			}],
+			text:['撒的好伐科技','看是否可','沙发上课的恢复了恢复绝对是','思考的好机会金慧','凡事都好看就'],
+		},
+		news: [{
+			title: 'news1',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的加哈卡士大夫很快收到回复看',
+		},{
+			title: 'news2',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'龙卷风地方的加哈卡回复看',
+		},{
+			title: 'news3',
+			cover: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			url: 'http://yuanzi-beijing.oss-cn-beijing.aliyuncs.com/cardImg/2014-07/00779288-5c64-41c0-b178-7075a22e1a2c',
+			description:'恢复开机撒的好伐就库是否反馈货架上的房间地方好家伙',
+		}]
+	};
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	// Load the current category filter
+	
+				
 	
 	// Render the view
-	view.render('index');
+	view.render('com/index');
 	
 };
