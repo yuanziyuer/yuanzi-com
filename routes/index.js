64fe66ddcb6694d0742bf714780ec014ba4086e4
crazyjingling <crazyjingling@126.com>
2015-11-24 20:09:07 +0800

    added: 增加民安保险的图，增加手机app关于元子界面

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 /**
  * This file is where you define your application routes and controllers.
- * 
+ *
  * Start by including the middleware you want to run for every request;
  * you can attach middleware to the pre('routes') and pre('render') events.
- * 
+ *
  * For simplicity, the default setup for route controllers is for each to be
  * in its own file, and we import all the files in the /routes/views directory.
- * 
+ *
  * Each of these files is a route controller, and is responsible for all the
  * processing that needs to happen for the route (e.g. loading data, handling
  * form submissions, rendering the view template, etc).
- * 
+ *
  * Bind each route pattern your application should respond to in the function
  * that is exported from this module, following the examples below.
- * 
+ *
  * See the Express application routing documentation for more information:
  * http://expressjs.com/api.html#app.VERB
  */
@@ -36,22 +36,27 @@ var routes = {
 exports = module.exports = function(app) {
 	// Views
 	app.get('/', routes.views.index);
-	
+
 	app.get('/cards', routes.views.cards);
-	
+
 	app.get('/cards/:cardId', routes.views.card);
-	
+
 	app.get('/apply', routes.views.apply);
 	app.post('/apply', routes.api.apply);
-	
+
 	app.get('/downloads', routes.views.downloads);
-	
+
 	app.get('/about', routes.views.about);
 
+	app.get('/aboutapp', routes.views.aboutApp);
+
 	app.get('/news/:newsId', routes.views.news);
-	
-	
+	app.get('*', function (req, res) {
+		res.redirect('/');
+	});
+
+
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
-	
+
 };
